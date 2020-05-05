dee6c2bab92759827d0666cb7b75be30b5d21844
Chase Nelson <nels0891@gmail.com>
2020-05-04 20:09:31 -0600

    Added new version of MOCK.csv that has correct data.

---
+++ mock_csv.rb
@@ -0,0 +1,10 @@
+# Write a script that generates the following reports:
+# - A table with everyone who has an email address and didn't meet their quota
+# - List of employees that are over 6 months employed
+# - Additional fields to the table that denote
+#   - who didn't meet their quote
+#   - average revenue per client (revenue / client number)
+#   - commission that needs to be paid out to each individual
+
+# First I need my program to open the CSV file. Read? Not sure.
+# 
